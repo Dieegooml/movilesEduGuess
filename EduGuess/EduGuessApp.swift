6be05ee50a5351f203aaa6050816e7d1afa2620f
Daniela2731 <daniela.salazar@tecsup.edu.pe>
2026-05-15 09:18:24 -0500

    Initial Commit

---
+++ EduGuess/EduGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduGuessApp.swift
+//  EduGuess
+//
+//  Created by Daniela Nicol Salazar Quina on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EduGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
